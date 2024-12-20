ddeeff9fdaf586164f230e1dde246151d939bfef
hjsong <dev_sklg0602@naver.com>
2024-12-20 17:05:20 +0900

    24.12.20 인덱스 실행계획, 생성

---
+++ 12_인덱스.sql
@@ -0,0 +1,39 @@
+-- cmd에서 'source employees.sql' 입력하여 employees 데이터베이스 생성
+
+-- 인덱스 실습
+-- 실행 계획 확인
+SELECT * FROM employees;
+EXPLAIN SELECT * FROM employees;
+
+SELECT * FROM employees WHERE emp_no = 48730;
+EXPLAIN SELECT * FROM employees WHERE emp_no = 48730;
+
+SELECT * FROM employees WHERE first_name = 'moon';
+EXPLAIN SELECT * FROM employees WHERE first_name = 'moon';
+
+-- 인덱스 생성
+CREATE INDEX idx_employees_first_name ON employees(first_name);
+
+ANALYZE TABLE employees;
+
+SELECT * FROM employees WHERE first_name = 'moon';
+EXPLAIN SELECT * FROM employees WHERE first_name = 'moon';
+
+CREATE INDEX idx_employees_first_name_last_name
+    ON employees(first_name, last_name);
+
+ANALYZE TABLE employees;
+
+SELECT *
+FROM employees
+WHERE first_name = 'moon'
+AND last_name = 'Yetto'
+;
+
+EXPLAIN SELECT * 
+FROM employees 
+WHERE first_name = 'moon' 
+  AND last_name = 'Yetto'
+;
+
+
