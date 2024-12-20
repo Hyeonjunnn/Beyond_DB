708739354343872d451af9953fc013d732ec0e07
hjsong <dev_sklg0602@naver.com>
2024-12-20 17:35:06 +0900

    24.12.20 인덱스 확인, 삭제, 예외

--- 12_인덱스.sql
+++ 12_인덱스.sql
@@ -38,4 +38,28 @@ WHERE first_name = 'moon'
   AND last_name = 'Yetto'
 ;
 
+-- 테이블에 지정된 인덱스 확인
+SHOW INDEX FROM employees;
 
+-- 인덱스 삭제
+DROP INDEX idx_employees_first_name ON employees;
+DROP INDEX idx_employees_first_name_last_name ON employees;
+
+-- 인덱스를 사용하지 않는 경우
+SELECT * FROM employees;
+EXPLAIN SELECT * FROM employees;
+
+SELECT * FROM employees WHERE emp_no < 250000;
+EXPLAIN SELECT * FROM employees WHERE emp_no < 250000;
+
+SELECT * FROM employees WHERE emp_no * 1= 100000;
+EXPLAIN SELECT * FROM employees WHERE emp_no * 1 = 100000;
+EXPLAIN SELECT * FROM employees WHERE emp_no / 1 = 100000;
+
+ALTER TABLE employees ADD INDEX idx_employees_gender(gender);
+ANALYZE TABLE employees;
+
+SELECT * FROM employees WHERE gender = 'M';
+EXPLAIN SELECT * FROM employees WHERE gender = 'M';
+
+ALTER TABLE employees DROP INDEX idx_employees_gender;
