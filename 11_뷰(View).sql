1f939ad8e8397d33174cc3b82bfb59858e5f66ce
hjsong <dev_sklg0602@naver.com>
2024-12-20 15:07:02 +0900

    24.12.20 뷰를 이용한 DML

--- 11_뷰(View).sql
+++ 11_뷰(View).sql
@@ -44,6 +44,111 @@ SELECT emp_name,
 		 gender
 FROM v_employee;
 
+-- 2. 뷰 수정
+-- 회원의 아이디, 이름, 구매 제품, 주소, 연락처를 조회하는 뷰를 생성
+CREATE VIEW v_userbuytbl
+AS (SELECT A.userId,
+		 	  A.uname,
+		 	  B.prodName,
+		 	  A.addr,
+		 	  CONCAT(A.mobile1, A.mobile2)
+	FROM usertbl A
+	LEFT OUTER JOIN buytbl B
+					 ON A.userID = B.userID
+	)
+;
+
+-- 뷰의 수정
+ALTER VIEW v_userbuytbl
+AS (SELECT A.userId,
+		 	  A.uname,
+		 	  B.prodName,
+		 	  A.addr,
+		 	  CONCAT(A.mobile1, A.mobile2) AS 'mobile'
+	FROM usertbl A
+	LEFT OUTER JOIN buytbl B
+					 ON A.userID = B.userID
+	)
+;
+
+-- 3. 뷰를 이용해서 DML(INSERT, UPDATE, DELETE) 사용
+CREATE VIEW v_job
+AS (SELECT *
+	 FROM job
+	)
+;
+
+-- SELECT
+SELECT job_code,
+		 job_name
+FROM v_job
+;
+
+-- INSERT
+INSERT INTO v_job 
+VALUES ('J8', '알바')
+;
+
+-- UPDATE
+UPDATE v_job
+SET job_name = '인턴'
+WHERE job_code = 'J8'
+;
+
+-- DELETE
+DELETE
+FROM v_job
+WHERE job_code = 'J8'
+;
+
+-- 4. DML 조작이 불가능한 경우
+-- 뷰 정의에 포함되지 않은 열을 조작하는 경우
+CREATE OR REPLACE VIEW v_job
+AS (SELECT job_code
+	 FROM job
+	)
+;
+
+-- INSERT
+INSERT INTO v_job 
+VALUES ('J8', '알바') -- 에러 발생
+;
+
+-- UPDATE
+UPDATE v_job
+SET job_name = '인턴'
+WHERE job_code = 'J7' -- 에러 발생
+;
+
+-- DELETE
+DELETE
+FROM v_job
+WHERE job_name = '사원' -- 에러 발생
+;
+
+-- 2) 산술 표현법으로 정의된 열을 조작하는 경우
+CREATE VIEW v_emp_salary
+AS (SELECT emp_id,
+			  emp_name,
+			  emp_no,
+			  salary * 12 AS 'salary'
+   FROM employee
+	)
+;
+
+-- INSERT
+INSERT INTO v_emp_salary
+VALUES ('300', '홍길동', '950525-1234567', 30000000) -- 에러 발생
+;
+
+-- UPDATE
+UPDATE v_emp_salary
+SET salary = 30000000
+WHERE emp_id = '200' -- 에러 발생
+;
+
+SELECT * FROM v_emp_salary;
+
 
 
 
